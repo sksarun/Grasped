2f679e9a1189808240d5fdb74c449eabd0fb60e3
Tong <Tong@Tongs-MacBook-Pro.local>
2012-01-28 16:44:44 +0700

    Initial Commit

---
+++ Grasped/com_tong_graspedViewController.h
@@ -0,0 +1,13 @@
+//
+//  com_tong_graspedViewController.h
+//  Grasped
+//
+//  Created by ศรัณย์ กฤษณะโลม on 1/28/55 BE.
+//  Copyright (c) 2555 Agoda.co.ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface com_tong_graspedViewController : UIViewController
+
+@end
---
+++ Grasped/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Grasped
+//
+//  Created by ศรัณย์ กฤษณะโลม on 1/28/55 BE.
+//  Copyright (c) 2555 Agoda.co.ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "com_tong_graspedAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([com_tong_graspedAppDelegate class]));
+    }
+}
